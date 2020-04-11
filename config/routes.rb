7c1521b287ad1a29df2d994f299905f4dbb321da
Ines Guerrero <inesgs@gmail.com>
2020-04-11 14:41:48 +0100

    fix messages route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :messages
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+        resources :messages
+    end 
+  end
   devise_for :users
 end
