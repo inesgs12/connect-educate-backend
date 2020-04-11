6c061c07f3612c597c9e633b8886592ef2fc68f8
rebecca fitzsimmons <rebecca@madetech.com>
2020-04-11 13:58:45 +0100

    Add method to seed volunteer data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  User.create(
+    email: "test#{i}@email.com",
+    encrypted_password: "password#{i}",
+    type: 'Volunteer',
+    first_name: "first_name_#{i}",
+    last_name: "last_name_#{i}",
+    bio: "I am volunteer ##{i} and I love connect-educate :D",
+    birth_date: "#{i}/12/1994" 
+  )
+end
