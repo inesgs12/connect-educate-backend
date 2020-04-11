89233b15661c207fab1580ecde9e850e24772463
Ines Guerrero <inesgs@gmail.com>
2020-04-11 13:37:49 +0100

    create Message

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,36 @@
+class MessagesController < ApplicationController
+
+    def index
+        messages = Message.all 
+        render json: messages
+    end
+
+    def show
+        message = Message.find_by(id: params[:id])
+        if message
+            render json: message
+        else
+            render json: { error: 'Message not found.' }, status: 404
+        end
+    end
+
+    def create
+        message = Message.new(message_params)
+        if message.valid?
+            message.save 
+            render json: message 
+        else 
+            render json: { error: "Validation failed: Please enter a message"}
+        end
+    end
+
+    private
+
+    # will we add a title to the message like email format or like chat format? 
+    # can we send attachments? 
+    
+    def message_params
+        params.permit(:content)
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_112324) do
+ActiveRecord::Schema.define(version: 2020_04_11_122714) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
