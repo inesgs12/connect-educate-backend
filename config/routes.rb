d9fc80eea5f979ee3045ab8ca3a5e42b89b27ce9
Michelle Brien <michelle.brien12@gmail.com>
2020-04-11 17:13:57 +0100

    adds generic sign in and out for users

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,20 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :volunteers,
+  devise_for :users,
             defaults: { format: :json},
             controllers: {
               sessions: 'api/v1/sessions',
+             }
+  devise_for :volunteers,
+            defaults: { format: :json},
+            controllers: {
               registrations: 'api/v1/registrations'
              }
 
   devise_for :parents,
              defaults: { format: :json},
              controllers: {
-               sessions: 'api/v1/sessions',
                registrations: 'api/v1/registrations'
               }
 end
