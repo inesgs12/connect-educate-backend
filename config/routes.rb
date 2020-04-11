2ee641cf21ee4de199a06c818d15b0eecc3375f2
Michelle Brien <michelle.brien12@gmail.com>
2020-04-11 17:01:40 +0100

    adds devise login for api

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,20 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :messages
       resources :volunteers
-    end 
+    end
   end
-  devise_for :users
+
+  devise_for :volunteers,
+            defaults: { format: :json},
+            controllers: {
+              sessions: 'api/v1/sessions',
+              registrations: 'api/v1/registrations'
+             }
+
+  devise_for :parents,
+             defaults: { format: :json},
+             controllers: {
+               sessions: 'api/v1/sessions',
+               registrations: 'api/v1/registrations'
+              }
 end
