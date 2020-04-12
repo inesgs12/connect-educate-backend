55205e622fb128bae443c93338978efdd938c807
Ines Guerrero <inesgs@gmail.com>
2020-04-12 17:19:40 +0100

    separete create parent and create task

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -18,36 +18,61 @@ module Api
                 end
             end
 
-            def create
-                task = Task.new(params[:task])
-                
-                if task.valid?
-                    task.save
-                    render json: task
+            def create_parent
+                parent = Parent.new(parent_params)
+
+                if parent.valid?
+                    parent.save
+                    create_task(parent, task_params)
+                    render json: parent
                 else
                     render json: {
-                        error: "Please fill out all required fields"
-                    }
-                end
-
-                user = User.new(params[:user])
-
-                if user.valid>
-                    user.save
-                    render json: user
-                else
-                    render json: {
-                        error: "User not saved"
+                        error: "Parent not saved"
                     }
                 end
             end
 
+            def create_task(parent, task_params)
+                # task = Task.new(parent_id: params[parent.id], content: params[task_params.content])
+                
+                task = parent.tasks.create!(task_params)
+                # if task.valid?
+                #     task.save
+                #     render json: task
+                # else
+                #     render json: {
+                #         error: "Please fill out all required fields"
+                #     }
+                # end
+
+            end
+
             private
 
-            # def task_params
-            #     # params.require(:content, :parent_id, :skill_id).permit(:volunteer_id, :task_language, :childs_age)
-            #     params.require(:task)
-            # end
+            def task_params
+                # params.require(:content, :parent_id, :skill_id).permit(:volunteer_id, :task_language, :childs_age)
+                params.require(task:%i[
+                    :content,
+                    :skill_id
+                ]).permit(task:%i[
+                    :childs_age,
+                    task_language,
+                ])
+            end
+
+            def parent_params
+                params.require(parent:%i[
+                    :first_name,
+                    :last_name,
+                    :email,
+                    :password,
+                ]).permit(parent:%i[
+                    :bio,
+                    :birth_date,
+                    :english_proficiency,
+                    :preferred_language
+                ])
+            end
 
         end
     end
