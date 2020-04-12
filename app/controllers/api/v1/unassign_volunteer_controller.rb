9d610df71f84a324090b54c4c8d33acec5d50c86
rebecca fitzsimmons <rebecca@madetech.com>
2020-04-12 17:12:42 +0100

    Set volunteer to nil when unaasigning from task

--- app/controllers/api/v1/unassign_volunteer_controller.rb
+++ app/controllers/api/v1/unassign_volunteer_controller.rb
@@ -1,12 +1,12 @@
 module Api
   module V1
-    class UnAssignVolunteerController < ApplicationController
+    class UnassignVolunteerController < ApplicationController
       def update
         task_id = params[:id]
         volunteer_id = params[:volunteer]
         task = Task.where(id: task_id).where(volunteer_id: volunteer_id)
-        if !task.empty
-          task.update(volunteer_id: null)
+        if !task.empty?
+          task.update(volunteer_id: nil)
         else
           render json: { error: 'Task not found.' }, status: 404
         end
