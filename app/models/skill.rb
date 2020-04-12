ad6542bec8e800d14e80b9121561da6ff2cf9680
rebecca fitzsimmons <rebecca@madetech.com>
2020-04-12 09:00:15 +0100

    Set name field to be enum in skill model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,5 @@
 class Skill < ApplicationRecord
   belongs_to :volunteer
+
+  enum name: { maths: 0, english: 1, physics: 2, chemistry: 3, biology: 4, geography: 5, history: 6, computer_science: 7 }
 end
