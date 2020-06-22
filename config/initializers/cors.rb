a3eef5ab9360d12ac35cbaecfbcec9e92496fcfe
Ines Guerrero <inesgs@gmail.com>
2020-06-22 18:28:40 +0100

    cors again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,6 +15,18 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 end
 
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://infinite-coast-05053.herokuapp.com/*'
+
+    resource '/api/*',
+      headers: %w(Authorization),
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: %w(Authorization),
+      max_age: 600
+  end
+end
+
 # config.middleware.insert_before 0, Rack::Cors do
 #   allow do
 #     origins 'http://localhost:3000'
