33d757196327ad8b2cadcdc654ba1b95b4f08849
Ines Guerrero <inesgs@gmail.com>
2020-06-22 19:38:44 +0100

    Revert "cors"
    This reverts commit e141a762923ac345e3efa97e52c393079d313023.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://infinite-coast-05053.herokuapp.com/'
+    origins '*'
+
     resource '*',
-      headers: :any,
+      headers: %w(Authorization),
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      expose: %w(Authorization),
+      max_age: 600
   end
 end
 
