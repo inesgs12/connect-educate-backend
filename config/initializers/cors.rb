820edc249a3552473f37d9ef3302a4505450e740
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-07-06 09:44:42 +0200

    reverted last commit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,12 +8,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-
-    resource(
-      '*',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    )
   end
 end
 
