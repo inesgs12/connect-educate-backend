6dc80d1c1daae25743d5b09dddd5256e88dd184c
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-07-06 09:52:04 +0200

    rack-cors documentation for rails 5 added in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,19 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
   allow do
     origins '*'
+
+    resource '/cors',
+      :headers => :any,
+      :methods => [:post],
+      :max_age => 0
+
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      :headers => :any,
+      :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+      :max_age => 0
   end
 end
 
